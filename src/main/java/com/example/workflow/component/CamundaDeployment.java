aebc3063d79838f4c02c1d09c6264e7aae3a1ede
jojo2283 <denisbermas04@mail.ru>
2025-04-24 13:25:34 +0300

    fix

--- src/main/java/com/example/workflow/component/CamundaDeployment.java
+++ src/main/java/com/example/workflow/component/CamundaDeployment.java
@@ -1,24 +1,21 @@
 package com.example.workflow.component;
 
-import jakarta.annotation.PostConstruct;
-import org.camunda.bpm.engine.RepositoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamundaDeployment {
 
-    @Autowired
-    private RepositoryService repositoryService;
-
-    @PostConstruct
-    public void deployProcessAndForm() {
-        repositoryService.createDeployment()
-                .name("course-process")
-                .addClasspathResource("blps.bpmn")
-                .addClasspathResource("static/forms/selectCourse.html")
-                .deploy();
-
-        System.out.println("✅ Deployed selectCourse.bpmn and selectCourse.form");
-    }
+//    @Autowired
+//    private RepositoryService repositoryService;
+//
+//    @PostConstruct
+//    public void deployProcessAndForm() {
+//        repositoryService.createDeployment()
+//                .name("course-process")
+//                .addClasspathResource("blps.bpmn")
+//                .addClasspathResource("static/forms/selectCourse.html")
+//                .deploy();
+//
+//        System.out.println("✅ Deployed selectCourse.bpmn and selectCourse.form");
+//    }
 }
