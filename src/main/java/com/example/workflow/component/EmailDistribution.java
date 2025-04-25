1e7955f56b3ffac3e82404143fc6de9118edc929
Dmitriy Shishmintsev <dmitriyshishmintsev@gmail.com>
2025-04-25 22:05:43 +0300

    email kafka

--- src/main/java/com/example/workflow/component/EmailDistribution.java
+++ src/main/java/com/example/workflow/component/EmailDistribution.java
@@ -1,7 +1,10 @@
 package com.example.workflow.component;
 
 import com.example.workflow.entities.User;
+import com.example.workflow.kafka.EmailRequest;
+import com.example.workflow.kafka.KafkaProducer;
 import com.example.workflow.repositories.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
@@ -14,6 +17,8 @@ import java.util.List;
 public class EmailDistribution implements JavaDelegate {
 
     private final  UserRepository userRepository;
+    private final ObjectMapper objectMapper;
+    private final KafkaProducer kafkaProducer;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
@@ -21,6 +26,13 @@ public class EmailDistribution implements JavaDelegate {
         List<User> users = userRepository.findByPointsGreaterThan(0);
         for (User user : users) {
             System.out.println("Шлем имейл на: " + user.getEmail());
+            String json = objectMapper.writeValueAsString(new EmailRequest(
+                    user.getEmail(),
+                    "добрый день",
+                    "пора бы потратить деньги. Их у вас целых " + user.getPoints()
+
+            ));
+            kafkaProducer.sendMessage(json);
         }
     }
 }
